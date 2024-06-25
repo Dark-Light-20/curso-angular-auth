a66df208b45a5aa87f487435ef7e2a7a606bde2b
Dark-Light-20 <pazsebas@hotmail.com>
2024-06-24 20:01:40 -0500

    feat: :sparkles: Login guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from '@guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
   {
     path: 'app',
-    loadChildren: () => import('./modules/layout/layout.module').then((m) => m.LayoutModule),
+    canActivate: [authGuard],
+    loadChildren: () =>
+      import('./modules/layout/layout.module').then((m) => m.LayoutModule),
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { environment } from '@environments/environment';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
+import { TokenService } from './token.service';
+import { ResponseLogin } from '@models/auth.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private readonly _http = inject(HttpClient);
+  private readonly _tokenService = inject(TokenService);
   private readonly _apiUrl = environment.API_URL;
 
   login(email: string, password: string) {
-    return this._http.post(`${this._apiUrl}/auth/login`, {
-      email,
-      password,
-    });
+    return this._http
+      .post<ResponseLogin>(`${this._apiUrl}/auth/login`, {
+        email,
+        password,
+      })
+      .pipe(
+        tap((response) => this._tokenService.saveToken(response.access_token))
+      );
   }
 
   register(name: string, email: string, password: string) {
